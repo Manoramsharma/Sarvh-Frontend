55e22eba176695994a85f9595218698bb55a0a0a
Sohan Bandary <bandarysohan24@gmail.com>
2021-09-08 10:00:05 +0530

    deploy test

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -11,6 +11,7 @@ export const getDataAPI = async (url, token) => {
 export const postDataAPI = async (url, post, token) => {
   const res = await axios.post(`${API}/api/${url}`, post, {
     headers: { Authorization: token },
+    withCredentials: true,
   });
 
   return res;
